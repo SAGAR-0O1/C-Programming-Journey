f3165f1d1426661249b2b844facc631dbc7dbcb4
SAGAR <sagar956817@gmail.com>
2026-07-08 14:50:58 +0530

    Update Number_Guessing_GAme.c

--- Number_Guessing_GAme.c
+++ Number_Guessing_GAme.c
@@ -1,28 +1,42 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-    int cn = 50;
-    int n, a = 0;
+    int cn, n, a = 0;
 
-    while(1)
+    // Initialize random number generator
+    srand(time(NULL));
+
+    // Generate a random number between 1 and 100
+    cn = rand() % 100 + 1;
+
+    printf("===== Number Guessing Game =====\n");
+    printf("Guess the number between 1 and 100.\n");
+
+    while (1)
     {
-        printf("Enter number: ");
+        printf("\nEnter your guess: ");
         scanf("%d", &n);
 
         a++;
 
-        if(n > cn)
+        if (n < 1 || n > 100)
         {
-            printf("Too High\n");
+            printf("Please enter a number between 1 and 100.\n");
         }
-        else if(n < cn)
+        else if (n > cn)
         {
-            printf("Too Low\n");
+            printf("Too High!\n");
+        }
+        else if (n < cn)
+        {
+            printf("Too Low!\n");
         }
         else
         {
-            printf("Correct Number\n");
+            printf("\nCongratulations! You guessed the correct number.\n");
             printf("Total Attempts = %d\n", a);
             break;
         }
